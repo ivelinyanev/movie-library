435570ea532027112913c537eca0ec7faa376f4c
ivelinyanev <123307555+ivelinyanev@users.noreply.github.com>
2025-12-12 11:45:30 +0200

    Add global exception handling

---
+++ src/main/java/movielibrary/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,123 @@
+package movielibrary.exceptions;
+
+import io.jsonwebtoken.JwtException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.nio.file.AccessDeniedException;
+import java.util.Date;
+
+import static movielibrary.utils.StringConstants.*;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public ErrorMessage handleEntityNotFoundException(EntityNotFoundException ex, WebRequest request) {
+
+        return new ErrorMessage(
+                HttpStatus.NOT_FOUND.value(),
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+    }
+
+    @ExceptionHandler(DuplicateEntityException.class)
+    @ResponseStatus(value = HttpStatus.CONFLICT)
+    public ErrorMessage handleDuplicateEntityException(DuplicateEntityException ex, WebRequest request) {
+
+        return new ErrorMessage(
+                HttpStatus.CONFLICT.value(),
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+    public ErrorMessage handleUsernameNotFoundException(UsernameNotFoundException ex, WebRequest request) {
+
+        return new ErrorMessage(
+                HttpStatus.UNAUTHORIZED.value(),
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    @ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+    public ErrorMessage handleUsernameNotFoundException(AuthenticationException ex, WebRequest request) {
+
+        return new ErrorMessage(
+                HttpStatus.UNAUTHORIZED.value(),
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(value = HttpStatus.FORBIDDEN)
+    public ErrorMessage handleAccessDeniedException(AccessDeniedException ex, WebRequest request) {
+
+        return new ErrorMessage(
+                HttpStatus.FORBIDDEN.value(),
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+    }
+
+    @ExceptionHandler(JwtException.class)
+    @ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+    public ErrorMessage handleJwtException(JwtException ex, WebRequest request) {
+
+        return new ErrorMessage(
+                HttpStatus.UNAUTHORIZED.value(),
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ErrorMessage handleHttpMessageNotReadableException(HttpMessageNotReadableException ex, WebRequest request) {
+
+        return new ErrorMessage(
+                HttpStatus.BAD_REQUEST.value(),
+                new Date(),
+                extractReadableMessage(ex),
+                request.getDescription(false));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ErrorMessage handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest request) {
+
+        return new ErrorMessage(
+                HttpStatus.BAD_REQUEST.value(),
+                new Date(),
+                extractReadableMessage(ex),
+                request.getDescription(false));
+    }
+
+    private String extractReadableMessage(Exception e) {
+        String message = e.getMessage();
+
+        if (message != null && message.contains(VALIDATION_FAILED)) {
+            return VALUES_INVALID_OR_NOT_WITHIN_REASONABLE_RANGE;
+        }
+        if (message != null && message.contains(DESERIALIZATION_FAILED)) {
+            return DESERIALIZATION_FAILED_UNEXPECTED_TYPE;
+        }
+
+        return REQUEST_BODY_COULD_NOT_BE_READ;
+    }
+
+}
--- src/main/java/movielibrary/utils/StringConstants.java
+++ src/main/java/movielibrary/utils/StringConstants.java
@@ -6,4 +6,9 @@ public class StringConstants {
     public static final String USERNAME_LIMITATION = "Username must be between 3 and 20 characters.";
     public static final String PASSWORD_LIMITATION = "Password must be between 3 and 50 characters.";
     public static final String YEAR_LIMITATION = "ReleaseYear must be between 1888 and next year";
+    public static final String VALIDATION_FAILED = "Validation failed for argument";
+    public static final String VALUES_INVALID_OR_NOT_WITHIN_REASONABLE_RANGE = "Values passed in request body are not valid or not within reasonable range.";
+    public static final String DESERIALIZATION_FAILED = "Cannot deserialize value of type";
+    public static final String DESERIALIZATION_FAILED_UNEXPECTED_TYPE = "Deserialization failed for a field. This happens when a value inside the request body is not of the expected type";
+    public static final String REQUEST_BODY_COULD_NOT_BE_READ = "Request body could not be read.";
 }
